b3d6beeb886e6601f805c69c10674f4768b760b1
Gourav-21 <rahul.dora21@gmail.com>
2024-03-11 15:11:52 +0530

    fixed index

--- next-frontend/src/pages/index.tsx
+++ next-frontend/src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     <div className="w-full h-full absolute ">
       <EvervaultCard className="z-0" text="WhitleBlower">
         <div className="flex flex-col items-center justify-center text-center h-full gap-2 z-20">
-          <h1 className="md:section-header-headline md:text-6xl text-5xl dark:text- mt-10  text-black z-20">WhitleBlower</h1>
+          <h1 className="md:section-header-headline md:text-6xl text-5xl dark:text- mt-10  text-black z-20">WhistleBlower</h1>
           <p className="p-10 text-gray-700 md:text-base text-sm w-2/3 md:w-full  select-none z-20">🕵️‍♂️ WhistleBlower is a platform for whistleblowers to report misconduct in a secure and private manner.</p>
           <ConnectWallet />
         </div>
