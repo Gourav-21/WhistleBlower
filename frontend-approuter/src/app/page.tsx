0fcef6b803b71248b3676c2427a42a091d38398b
Gourav-21 <rahul.dora21@gmail.com>
2024-03-17 22:29:24 +0530

    added metadate and page data

--- frontend-approuter/src/app/page.tsx
+++ frontend-approuter/src/app/page.tsx
@@ -1,7 +1,27 @@
+import ConnectWallet from "@/components/ConnectWallet";
+import { generateRandomUsername } from "@/components/functions";
+import { EvervaultCard } from "@/components/ui/evervault-card";
+import { useEffect } from "react";
+
 export default function Home() {
+
+  useEffect(() => {
+    if(localStorage.getItem('SecretName') === null) {
+      const randomUsername = generateRandomUsername();
+      localStorage.setItem('SecretName', randomUsername)
+    }
+    console.log(localStorage.getItem('SecretName'))
+  }, [])
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="w-full h-full absolute ">
+      <EvervaultCard className="z-0" text="WhitleBlower">
+        <div className="flex flex-col items-center justify-center text-center h-full gap-2 z-20">
+          <h1 className="md:section-header-headline md:text-6xl text-5xl dark:text- mt-10  text-black z-20">WhistleBlower</h1>
+          <p className="p-10 text-gray-700 md:text-base text-sm w-2/3 md:w-full  select-none z-20">🕵️‍♂️ WhistleBlower is a platform for whistleblowers to report misconduct in a secure and private manner.</p>
+          <ConnectWallet />
+        </div>
+      </EvervaultCard>
     </div>
   );
 }
