329fe554a74426d4b9521f3376ee570f7e6bc980
Gourav-21 <rahul.dora21@gmail.com>
2024-03-19 06:52:19 +0530

    no cache

--- next-frontend/src/app/post/page.tsx
+++ next-frontend/src/app/post/page.tsx
@@ -1,10 +1,10 @@
-import axios from "axios";
 import Post from "./post";
 
 async function getpost(){
-  const res = await axios.get("https://secretwhistleblower.vercel.app/api/posts")
-  return res.data.posts;
-}
+  const res = await fetch('https://secretwhistleblower.vercel.app/api/posts', { cache: 'no-store' })
+  const data= await res.json();
+  return data.posts;
+};
 
 export default async function Page() {
   const posts = await getpost();
