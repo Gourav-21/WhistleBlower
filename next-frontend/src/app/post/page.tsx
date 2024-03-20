32895017a1885104e1d23ea328c1015d48855a87
Gourav-21 <rahul.dora21@gmail.com>
2024-03-20 17:04:39 +0530

    no store

--- next-frontend/src/app/post/page.tsx
+++ next-frontend/src/app/post/page.tsx
@@ -5,72 +5,72 @@ import { SecretNetworkClient } from "secretjs";
 import Post from "./post";
 import { unstable_cache, unstable_noStore } from "next/cache";
 
-type metadata={
-  _id:string,
-  date:string,
-  vote:Number,
-  comments:[]
-}
-
-interface data{
-	posts:post[]
-}
-
-const query = async (query) => {
-
-  let secretjs = new SecretNetworkClient({
-    chainId: "pulsar-3",
-    url: "https://api.pulsar.scrttestnet.com",
-  });
-
-  const my_query:data = await secretjs.query.compute.queryContract({
-    contract_address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-    code_hash: process.env.NEXT_PUBLIC_CONTRACT_HASH,
-    query: query,
-  });
-  
-  return my_query;
-};
-
-async function getpost() {
-  return await post();
-};
-
-const post = unstable_cache(
-  async () => {
-    await dbConnect();
-    const msg = { get_post: {} }
-    const posts: data = await query(msg)
-
-    const metadata:metadata[] = await POST.find().populate('comments');
-    
-    const mergedData = posts.posts.map(post => {
-    const matchedData:metadata = metadata.find(data => data.date === post.date);
-    const result = {
-      id: matchedData._id,
-      date: post.date,
-      title: post.title,
-      description: post.description,
-      vote: matchedData.vote,
-      comments: matchedData.comments,
-    };
-    return result;
-  });
-  return JSON.stringify(mergedData);
-  },
-  ['posts'],
-  { tags: ['posts'] }
-);
-
-// async function getpost() {
-//   const res = await fetch('https://secretwhistleblower.vercel.app/api/posts', { next: { tags: ['posts'] }})
-//   const data= await res.json();
-//   return data.posts;
+// type metadata={
+//   _id:string,
+//   date:string,
+//   vote:Number,
+//   comments:[]
 // }
 
+// interface data{
+// 	posts:post[]
+// }
+
+// const query = async (query) => {
+
+//   let secretjs = new SecretNetworkClient({
+//     chainId: "pulsar-3",
+//     url: "https://api.pulsar.scrttestnet.com",
+//   });
+
+//   const my_query:data = await secretjs.query.compute.queryContract({
+//     contract_address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+//     code_hash: process.env.NEXT_PUBLIC_CONTRACT_HASH,
+//     query: query,
+//   });
+  
+//   return my_query;
+// };
+
+// async function getpost() {
+//   return await post();
+// };
+
+// const post = unstable_cache(
+//   async () => {
+//     await dbConnect();
+//     const msg = { get_post: {} }
+//     const posts: data = await query(msg)
+
+//     const metadata:metadata[] = await POST.find().populate('comments');
+    
+//     const mergedData = posts.posts.map(post => {
+//     const matchedData:metadata = metadata.find(data => data.date === post.date);
+//     const result = {
+//       id: matchedData._id,
+//       date: post.date,
+//       title: post.title,
+//       description: post.description,
+//       vote: matchedData.vote,
+//       comments: matchedData.comments,
+//     };
+//     return result;
+//   });
+//   return JSON.stringify(mergedData);
+//   },
+//   ['posts'],
+//   { tags: ['posts'] }
+// );
+
+async function getpost() {
+  const res = await fetch('https://secretwhistleblower.vercel.app/api/posts', { next: { tags: ['posts'] }})
+  const data= await res.json();
+  return data.posts;
+}
+
 export default async function Page() {
-  const data = await getpost();
-  const post=JSON.parse(data)
+  const post = await getpost();
+  // const post=JSON.parse(data)
   return (
     <div>
       <Post posts={post} />
